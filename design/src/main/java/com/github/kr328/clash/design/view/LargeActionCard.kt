0a574e6a1ef4ac8011a696afe36850e8b0150a0e
Live-Block <liveblock@qq.com>
2025-07-17 16:36:25 +0800

    fix: Build error

--- design/src/main/java/com/github/kr328/clash/design/view/LargeActionCard.kt
+++ design/src/main/java/com/github/kr328/clash/design/view/LargeActionCard.kt
@@ -60,6 +60,5 @@ class LargeActionCard @JvmOverloads constructor(
         minimumHeight = context.getPixels(R.dimen.large_action_card_min_height)
         radius = context.getPixels(R.dimen.large_action_card_radius).toFloat()
         elevation = context.getPixels(R.dimen.large_action_card_elevation).toFloat()
-        setCardBackgroundColor(context.resolveThemedColor(R.attr.colorSurface))
     }
 }
